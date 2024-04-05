77b73ba4bcf8770fdb1d0810ff14e90df3bd0484
Valery Balakishiev <valerabalakishiev@gmail.com>
2024-04-05 15:46:06 +0300

    add dev branch

---
+++ settings.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent
+SOURCE_DIR = ROOT_DIR / 'src'
+TRAIN_DIR = ROOT_DIR / 'src' / 'train'
+MODELS_DIR = ROOT_DIR / 'models'
+DATASET_DIR = ROOT_DIR / 'datasets'
